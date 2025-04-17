8ec227a8ecae11dea02e88b9052953c85c8d1072
tgsat <tgsat@users.noreply.github.com>
2025-04-17 06:46:28 +0000

    deploy: d54f92a76c84d9cd9ea6257c876d3dc85ff1a880

--- lib/utils/values/validation.dart
+++
@@ -1,8 +0,0 @@
-class Validation {
-  static String? validateEmptyText(String? field, String? value) {
-    if (value == null || value.isEmpty) {
-      return '$field tidak boleh kosong.';
-    }
-    return null;
-  }
-}
